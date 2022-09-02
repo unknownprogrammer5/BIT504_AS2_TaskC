0da7e54a954894066fd63fc90dc674e1d2325650
jacksonwallace@myop.ac.nz <jacks@LAPTOP-DPUBAV4K.hub>
2022-09-02 13:33:03 +1200

    Added Class comment

--- pong/src/pong/Ball.java
+++ pong/src/pong/Ball.java
@@ -3,6 +3,7 @@ package pong;
 import java.awt.Color;
 
 public class Ball extends Sprite {
+	//Changed ball width, height and colour for experiment1 branch
 	private static final int BALL_WIDTH = 20;
 	private static final int BALL_HEIGHT = 20;
 	private static final Color BALL_COLOUR = Color.RED;
